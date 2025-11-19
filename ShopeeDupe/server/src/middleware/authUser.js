595a13061ba6902b3f29eb54118c1d14fd60c171
Anh-Tuan04 <tuan.vuatuan2k4@hcmut.edu.vn>
2025-11-19 14:45:51 +0700

    Change a bit 6th

--- ShopeeDupe/server/src/middleware/authUser.js
+++
@@ -1,5 +0,0 @@
-export const authAdmin = (req, res, next) => {
-    // Authentication logic for admin users
-    console.log('Hi you are accessing admin route');
-    next();
-};
--- ShopeeDupe/server/src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUsers, postUser } from '../controllers/userController.js';
-import { authAdmin } from '../middleware/authUser.js';
-const userRoute = express.Router();
-
-userRoute.get('', authAdmin, getUsers);
-userRoute.post('', postUser);
-
-export default userRoute;
