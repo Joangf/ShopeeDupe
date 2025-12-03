0027156238740f398c02c9ece2a9cdc4a6fb1dd0
Joangf <nguyenhoang1362005@gmail.com>
2025-12-03 21:21:30 +0700

    Fix the effect of Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,35 +2,34 @@ import ProductGrid from "../components/Home/ProductGrid";
 import Navbar from "../components/Home/Navbar";
 import InfiniteScroll from "react-infinite-scroll-component";
 const API_URL = import.meta.env.VITE_BACKEND_URL;
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 const Home = ({ setIsLoggedIn, isLoggedIn }) => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-  const isFirst = useRef(true);
 
-  const fetchProducts = async (pageNumber) => {
-    try {
-      const response = await fetch(`${API_URL}/products?page=${pageNumber}`);
-      if (response.ok) {
-        const data = await response.json();
-        const products = data.products;
-        if (products.length === 0) {
-          setHasMore(false);
-        } else {
-          setProducts((prevProducts) => [...prevProducts, ...products]);
-        }
-      }
-    } catch (error) {
-      console.error("Failed to fetch products:", error);
-    }
-  };
   useEffect(() => {
-    if (isFirst.current) {
-      isFirst.current = false;
-      return;
-    }
+    let isMounted = true;
+    const fetchProducts = async (pageNumber) => {
+      try {
+        const response = await fetch(`${API_URL}/products?page=${pageNumber}`);
+        if (response.ok && isMounted) {
+          const data = await response.json();
+          const products = data.products;
+          if (products.length === 0) {
+            setHasMore(false);
+          } else {
+            setProducts((prevProducts) => [...prevProducts, ...products]);
+          }
+        }
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+      }
+    };
     fetchProducts(page);
+    return () => {
+      isMounted = false;
+    };
   }, [page]);
   return (
     <>
